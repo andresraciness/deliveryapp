de2e96ba398027530e32a221d622416d1407a68f
RacinesAndres <andres.racines@ibcaribe.com>
2021-11-24 18:03:26 -0500

    Displaying products with images in gridview

--- lib/theming_and_state_management/presentation/home/products/products_screen.dart
+++ lib/theming_and_state_management/presentation/home/products/products_screen.dart
@@ -1,3 +1,4 @@
+import 'package:deliveryapp/theming_and_state_management/data/in_memory_products.dart';
 import 'package:flutter/material.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -10,13 +11,29 @@ class ProductsScreen extends StatelessWidget {
         title: Text('Products'),
       ),
       body: GridView.builder(
-        itemCount: 4,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) => Card(
-          child: Text('Hamburger'),
-        ),
-      ),
+          itemCount: products.length,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            final product = products[index];
+            return _ItemProduct(product: product);
+          }),
     );
+  }
+}
+
+class _ItemProduct extends StatelessWidget {
+  final product;
+  const _ItemProduct({Key? key, this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: 
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        Image.asset(product.image)
+      ],),
+    ));
   }
 }
